6392a0ed42e53044f73b393c950b0f9a256fb485
urlaubaitos <austin@itos.fi>
2025-01-12 18:50:27 -0500

    build + special case island range

--- src/Burve.sol
+++ src/Burve.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IUniswapV3Pool} from "./integrations/kodiak/IUniswapV3Pool.sol";
-import {TransferHelper} from "./TransferHelper.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IUniswapV3Pool } from "./integrations/kodiak/IUniswapV3Pool.sol";
+import { TransferHelper } from "./TransferHelper.sol";
+import { IKodiakIsland } from "./integrations/kodiak/IKodiakIsland.sol";
+import { LiquidityAmounts } from "./integrations/uniswap/LiquidityAmounts.sol";
+import { TickMath } from "./integrations/uniswap/TickMath.sol";
+
+using TickRangeImpl for TickRange global;
 
 /// Defines the tick range of an AMM position.
 struct TickRange {
@@ -11,6 +16,16 @@ struct TickRange {
     int24 lower;
     /// Upper tick of the range.
     int24 upper;
+}
+
+/// Implementation library for TickRange.
+library TickRangeImpl {
+    /// @notice Checks whether the given range is encoded to represent the island.
+    /// @param range The range to check.
+    /// @return isIsland True if the range is for an island.
+    function isIsland(TickRange memory range) internal pure returns (bool) {
+        return range.lower == 0 && range.upper == 0;
+    }
 }
 
 contract Burve is ERC20 {
@@ -32,6 +47,12 @@ contract Burve is ERC20 {
     /// Thrown when island specific logic is invoked but the contract was not initialized with an island.
     error NoIsland();
 
+    /// Thrown when the provided island points to a pool that does not match the provided pool.
+    error MismatchedIslandPool(address island, address pool);
+
+    /// Thrown in the consturctor if the supplied pool address is the zero address.
+    error PoolIsZeroAddress();
+
     /// Thrown when the number of ranges and number of weights do not match.
     error MismatchedRangeWeightLengths(
         uint256 rangeLength,
@@ -42,55 +63,44 @@ contract Burve is ERC20 {
     /// Please split up into multiple calls.
     error TooMuchBurnedAtOnce(uint128 liq, uint256 tokens, bool isX);
 
-    /// @param _island The island we are wrapping
-    /// @param _ranges the n ranges
-    /// @param _weights 1 + n weights defining the relative liquidity for each range. 
-    ///         The first weight at index 0 is for the island.
-    constructor(
-        address _island,
-        TickRange[] memory _ranges,
-        uint128[] memory _weights
-    ) ERC20(nameFromIsland(_island), symbolFromIsland(_island)) {
-        island = IKodiakIsland(_island);
-        pool = island.pool();
-
-        if (_ranges.length + 1 != _weights.length) {
-            revert MismatchedRangeWeightLengths(
-                _ranges.length + 1,
-                _weights.length
-            );
-        }
-
-        _init(_ranges, _weights);
-    }
-
     /// @param _pool The pool we are wrapping
+    /// @param _island The optional island we are wrapping
     /// @param _ranges the n ranges
     /// @param _weights n weights defining the relative liquidity for each range.
     constructor(
         address _pool,
+        address _island,
         TickRange[] memory _ranges,
         uint128[] memory _weights
     ) ERC20(nameFromPool(_pool), symbolFromPool(_pool)) {
         pool = IUniswapV3Pool(_pool);
+        token0 = pool.token0();
+        token1 = pool.token1();
 
-        if (_ranges.length != _weights.length)
+        island = IKodiakIsland(_island);
+
+        if (_pool == address(0x0)) {
+            revert PoolIsZeroAddress();
+        }
+
+        if (_island != address(0x0) && address(island.pool()) != _pool) {
+            revert MismatchedIslandPool(_island, _pool);
+        }
+
+        if (_ranges.length != _weights.length) {
             revert MismatchedRangeWeightLengths(
                 _ranges.length,
                 _weights.length
             );
-
-        _init(_ranges, _weights);
-    }
-
-    /// @notice initializes shared state between the two constructors.
-    function _init(TickRange[] memory _ranges, uint128[] memory _weights) private {
-        token0 = pool.token0();
-        token1 = pool.token1();
+        }
 
         // copy ranges to storage
         for (uint256 i = 0; i < _ranges.length; ++i) {
             ranges.push(_ranges[i]);
+
+            if (_ranges[i].isIsland() && address(island) == address(0x0)) {
+                revert NoIsland();
+            }
         }
 
         // compute total sum of weights
@@ -107,75 +117,72 @@ contract Burve is ERC20 {
 
     /// @notice mints liquidity for the recipient
     function mint(address recipient, uint128 liq) external {
-        uint256 i = 0; 
-
-        // mint the island
-        if (island != address(0)) {
+        for (uint256 i = 0; i < distX96.length; ++i) {
             uint128 liqAmount = uint128(shift96(liq * distX96[i], true));
-            uint256 shares = islandLiqToShares(liqAmount);
-            island.mint(shares, recipient);
-
-            ++i;
-        }
-
-        // mint the V3 ranges
-        while (i < distX96.length) {
-            TickRange memory range = ranges[i];
-            uint128 liqAmount = uint128(shift96(liq * distX96[i], true));
-
-            innerPool.mint(
-                address(this),
-                range.lower,
-                range.upper,
-                liqAmount,
-                abi.encode(msg.sender)
-            );
-
-            ++i;
+            mintRange(ranges[i], recipient, liqAmount);
         }
 
         _mint(recipient, liq);
+    }
+
+    /// @notice Helper method for minting to the given range. 
+    /// Used to decipher between the island and v3 ranges.
+    /// @param range The range to mint to.
+    /// @param recipient The recipient of the minted liquidity.
+    /// @param liq The amount of liquidity to mint.
+    function mintRange(TickRange memory range, address recipient, uint128 liq) internal {
+        // mint the island
+        if (range.lower == 0 && range.upper == 0) {
+            uint256 mintShares = islandLiqToShares(liq);
+            island.mint(mintShares, recipient);
+        } else {
+            // mint the V3 ranges
+            pool.mint(
+                address(this),
+                range.lower,
+                range.upper,
+                liq,
+                abi.encode(msg.sender)
+            );
+        }
     }
 
     /// @notice burns liquidity for the msg.sender
     function burn(uint128 liq) external {
         _burn(msg.sender, liq);
 
-        uint256 i = 0;
-
-        // burn the island
-        if (island != address(0x0)) {
+        for (uint256 i = 0; i < distX96.length; ++i) {
             uint128 liqAmount = uint128(shift96(liq * distX96[i], true));
-            uint256 shares = islandLiqToShares(liqAmount);
-            island.burnAmount(shares, msg.sender);
-
-            ++i;
+            burnRange(ranges[i], liqAmount);
         }
+    }
 
-        // burn the V3 ranges
-        while (i < distX96.length) {
-            TickRange memory range = ranges[i];
-            uint128 amount = uint128(shift96(liq * distX96[i], false));
-
-            (uint256 x, uint256 y) = innerPool.burn(
+    /// @notice Helper method for burning from the given range.
+    /// Used to decipher between the island and v3 ranges.
+    /// @param range The range to burn from.
+    /// @param liq The amount of liquidity to burn.
+    function burnRange(TickRange memory range, uint128 liq) internal {
+        if (range.lower == 0 && range.upper == 0) {
+            uint256 burnShares = islandLiqToShares(liq);
+            island.burn(burnShares, msg.sender);
+        } else {
+            (uint256 x, uint256 y) = pool.burn(
                 range.lower,
                 range.upper,
-                amount
+                liq
             );
 
             if (x > type(uint128).max) revert TooMuchBurnedAtOnce(liq, x, true);
             if (y > type(uint128).max)
                 revert TooMuchBurnedAtOnce(liq, y, false);
 
-            innerPool.collect(
+            pool.collect(
                 msg.sender,
                 range.lower,
                 range.upper,
                 uint128(x),
                 uint128(y)
             );
-
-            ++i;
         }
     }
 
@@ -190,22 +197,23 @@ contract Burve is ERC20 {
         TransferHelper.safeTransferFrom(
             token0,
             source,
-            address(innerPool),
+            address(pool),
             amount0Owed
         );
         TransferHelper.safeTransferFrom(
             token1,
             source,
-            address(innerPool),
+            address(pool),
             amount1Owed
         );
     }
 
     /* internal helpers */
 
-    /// @notice Calculates the amount of shares in the island for the given liquidity.
-    /// @notice Calculates the 
-    function islandLiqToShares(uint128 liq) internal returns (uint256 shares) {
+    /// @notice Calculates the amount of shares for an island given the liquidity.
+    /// @param liq The liquidity to convert to shares.
+    /// @return shares The amount of shares.
+    function islandLiqToShares(uint128 liq) internal view returns (uint256 shares) {
         if (address(island) == address(0x0)) {
             revert NoIsland();
         }
@@ -216,8 +224,7 @@ contract Burve is ERC20 {
             sqrtRatioX96,
             island.lowerTick(),
             island.upperTick(),
-            islandLiq,
-            false
+            liq
         );
 
         (,, shares) = island.getMintAmounts(amount0, amount1);
@@ -232,7 +239,7 @@ contract Burve is ERC20 {
         uint160 sqrtRatioX96,
         int24 tickLower,
         int24 tickUpper,
-        uint128 liquidity,
+        uint128 liquidity
     ) internal pure returns (uint256 amount0, uint256 amount1) {
         uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(tickLower);
         uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(tickUpper);
@@ -254,13 +261,13 @@ contract Burve is ERC20 {
     }
 
     /// @notice Computes the name for the ERC20 token given the pool address.
-    /// @param pool The pool address.
+    /// @param _pool The pool address.
     /// @return name The name of the ERC20 token.
     function nameFromPool(
-        address pool
+        address _pool
     ) private view returns (string memory name) {
-        address t0 = IUniswapV3Pool(pool).token0();
-        address t1 = IUniswapV3Pool(pool).token1();
+        address t0 = IUniswapV3Pool(_pool).token0();
+        address t1 = IUniswapV3Pool(_pool).token1();
         name = string.concat(
             ERC20(t0).name(),
             "-",
@@ -270,36 +277,18 @@ contract Burve is ERC20 {
     }
 
     /// @notice Computes the symbol for the ERC20 token given the pool address.
-    /// @param pool The pool address.
+    /// @param _pool The pool address.
     /// @return sym The symbol of the ERC20 token.
     function symbolFromPool(
-        address pool
+        address _pool
     ) private view returns (string memory sym) {
-        address t0 = IUniswapV3Pool(pool).token0();
-        address t1 = IUniswapV3Pool(pool).token1();
+        address t0 = IUniswapV3Pool(_pool).token0();
+        address t1 = IUniswapV3Pool(_pool).token1();
         sym = string.concat(
             ERC20(t0).symbol(),
             "-",
             ERC20(t1).symbol(),
             "-SLP-KDK"
         );
-    }
-
-    /// @notice Computes the name for the ERC20 token given the island address.
-    /// @param island The island address.
-    /// @return name The name of the ERC20 token.
-    function nameFromIsland(
-        address island
-    ) private view returns (string memory name) {
-        return nameFromPool(IKodiakIsland(island).pool());
-    }
-
-    /// @notice Computes the symbol for the ERC20 token given the island address.
-    /// @param island The island address.
-    /// @return sym The symbol of the ERC20 token.
-    function symbolFromIsland(
-        address island
-    ) private view returns (string memory sym) {
-        symbolFromPool(IKodiakIsland(island).pool());
     }
 }
