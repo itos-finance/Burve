3a67d7d0600e36974509ea9823e778e86ccc0d48
urlaubaitos <austin@itos.fi>
2025-02-05 18:10:04 -0500

    finish mint tests

--- test/stable/Burve.t.sol
+++ test/stable/Burve.t.sol
@@ -23,6 +23,8 @@ contract BurveTest is ForkableTest {
     IERC20 token0;
     IERC20 token1;
 
+    uint256 private constant X96MASK = (1 << 96) - 1;
+
     function forkSetup() internal virtual override {
         // Pool info
         pool = IUniswapV3Pool(
@@ -96,13 +98,12 @@ contract BurveTest is ForkableTest {
     }
 
     // Tests
-    // - mint
     // - burn
     // - getInfo
 
     // Contructor Tests
 
-    function testBurveIslandSetup() public view forkOnly {
+    function testIslandSetup() public view forkOnly {
         assertEq(address(burveIsland.pool()), address(pool), "burveIsland pool address");
         assertEq(address(burveIsland.token0()), address(token0), "burveIsland token0 address");
         assertEq(address(burveIsland.token1()), address(token1), "burveIsland token1 address");
@@ -120,7 +121,7 @@ contract BurveTest is ForkableTest {
         ); // 1/1
     }
 
-    function testBurveV3Setup() public view forkOnly {
+    function testV3Setup() public view forkOnly {
         assertEq(address(burveV3.pool()), address(pool), "burveV3 pool address");
         assertEq(address(burveV3.token0()), address(token0), "burveV3 token0 address");
         assertEq(address(burveV3.token1()), address(token1), "burveV3 token1 address");
@@ -142,7 +143,7 @@ contract BurveTest is ForkableTest {
         ); // 1/1
     }
 
-    function testBurveSetup() public view forkOnly {
+    function testSetup() public view forkOnly {
         assertEq(address(burve.pool()), address(pool), "burve pool address");
         assertEq(address(burve.token0()), address(token0), "burve token0 address");
         assertEq(address(burve.token1()), address(token1), "burve token1 address");
@@ -255,12 +256,6 @@ contract BurveTest is ForkableTest {
 
     // Mint Tests
 
-    // island mint
-    // - msg.sender is approved address / user 
-    // - token0 / token1 amounts transfered from msg.sender 
-    // - Burve LP token sent to recipient 
-    // - Island LP token sent to recipient 
-
     function testIslandMintSenderIsRecipient() public {
         address user = address(0xabc);
         uint128 liq = 10_000;
@@ -325,31 +320,6 @@ contract BurveTest is ForkableTest {
     }
 
     function testV3MintSenderIsRecipient() public {
-        address sender = address(this);
-        address user = address(0xabc);
-        uint128 liq = 10_000;
-
-        (int24 lower, int24 upper) = burveV3.ranges(0);
-        (uint256 mint0, uint256 mint1) = getAmountsFromRangeLiquidity(TickRange(lower, upper), liq);
-
-        deal(address(token0), address(sender), mint0);
-        deal(address(token1), address(sender), mint1);
-    
-        token0.approve(address(burveV3), mint0);
-        token1.approve(address(burveV3), mint1);
-
-        burveV3.mint(address(user), liq);
-
-        assertEq(token0.balanceOf(address(sender)), 0, "sende token0 balance");
-        assertEq(token1.balanceOf(address(sender)), 0, "sende token1 balance");
-        assertEq(
-            IERC20(burveV3).balanceOf(user),
-            liq,
-            "user burve LP balance"
-        );
-    }
-
-    function testV3MintSenderNotRecipient() public {
         address user = address(0xabc);
         uint128 liq = 10_000;
 
@@ -377,159 +347,117 @@ contract BurveTest is ForkableTest {
         );
     }
 
+    function testV3MintSenderNotRecipient() public {
+        address sender = address(this);
+        address user = address(0xabc);
+        uint128 liq = 10_000;
+
+        (int24 lower, int24 upper) = burveV3.ranges(0);
+        (uint256 mint0, uint256 mint1) = getAmountsFromRangeLiquidity(TickRange(lower, upper), liq);
+
+        deal(address(token0), address(sender), mint0);
+        deal(address(token1), address(sender), mint1);
+    
+        token0.approve(address(burveV3), mint0);
+        token1.approve(address(burveV3), mint1);
+
+        burveV3.mint(address(user), liq);
+
+        assertEq(token0.balanceOf(address(sender)), 0, "sende token0 balance");
+        assertEq(token1.balanceOf(address(sender)), 0, "sende token1 balance");
+        assertEq(
+            IERC20(burveV3).balanceOf(user),
+            liq,
+            "user burve LP balance"
+        );
+    }
+
+    function testMintSenderIsRecipient() public {
+        address user = address(0xabc);
+        uint128 liq = 10_000;
+
+        // island liq
+        uint128 islandLiq = uint128(shift96(liq * burve.distX96(0), true));
+        (uint256 islandMint0, uint256 islandMint1, uint256 islandMintShares) = LiquidityCalculations
+            .getAmountsFromIslandLiquidity(burve.island(), islandLiq);
+
+        // v3 liq
+        uint128 v3Liq = uint128(shift96(liq * burve.distX96(1), true));
+        (int24 lower, int24 upper) = burve.ranges(1);
+        (uint256 v3Mint0, uint256 v3Mint1) = getAmountsFromRangeLiquidity(TickRange(lower, upper), v3Liq);
+
+        uint256 mint0 = islandMint0 + v3Mint0;
+        uint256 mint1 = islandMint1 + v3Mint1;
+
+        deal(address(token0), address(user), mint0);
+        deal(address(token1), address(user), mint1);
+
+        vm.startPrank(user);
+    
+        token0.approve(address(burve), mint0);
+        token1.approve(address(burve), mint1);
+
+        burve.mint(address(user), liq);
+
+        vm.stopPrank();
+
+        assertEq(token0.balanceOf(address(user)), 0, "user token0 balance");
+        assertEq(token1.balanceOf(address(user)), 0, "user token1 balance");
+        assertEq(
+            IERC20(burve.island()).balanceOf(user),
+            islandMintShares,
+            "user island LP balance"
+        );
+        assertEq(
+            IERC20(burve).balanceOf(user),
+            liq,
+            "user burve LP balance"
+        );
+    }
+
+    function testMintSenderNotRecipient() public {
+        address sender = address(this);
+        address user = address(0xabc);
+        uint128 liq = 10_000;
+
+        // island liq
+        uint128 islandLiq = uint128(shift96(liq * burve.distX96(0), true));
+        (uint256 islandMint0, uint256 islandMint1, uint256 islandMintShares) = LiquidityCalculations
+            .getAmountsFromIslandLiquidity(burve.island(), islandLiq);
+
+        // v3 liq
+        uint128 v3Liq = uint128(shift96(liq * burve.distX96(1), true));
+        (int24 lower, int24 upper) = burve.ranges(1);
+        (uint256 v3Mint0, uint256 v3Mint1) = getAmountsFromRangeLiquidity(TickRange(lower, upper), v3Liq);
+
+        uint256 mint0 = islandMint0 + v3Mint0;
+        uint256 mint1 = islandMint1 + v3Mint1;
+
+        deal(address(token0), address(sender), mint0);
+        deal(address(token1), address(sender), mint1);
+    
+        token0.approve(address(burve), mint0);
+        token1.approve(address(burve), mint1);
+
+        burve.mint(address(user), liq);
+
+        assertEq(token0.balanceOf(address(sender)), 0, "sender token0 balance");
+        assertEq(token1.balanceOf(address(sender)), 0, "sender token1 balance");
+        assertEq(
+            IERC20(burve.island()).balanceOf(user),
+            islandMintShares,
+            "user island LP balance"
+        );
+        assertEq(
+            IERC20(burve).balanceOf(user),
+            liq,
+            "user burve LP balance"
+        );
+    }
+
     // island burn
     // ERC20(address(burveIsland.island())).approve(address(burveIsland), 10_000e18);
     // burveIsland.burn(1000);
-
-    function testV3Mint() public {
-        IUniswapV3Pool uniPool = IUniswapV3Pool(
-            BartioAddresses.KODIAK_HONEY_NECT_POOL_V3
-        );
-
-        address user = address(0xabc);
-
-        deal(uniPool.token0(), address(user), 10_000e18);
-        deal(uniPool.token1(), address(user), 10_000e18);
-
-        vm.startPrank(user);
-
-        IERC20(uniPool.token0()).approve(address(burveV3), 10_000e18);
-        IERC20(uniPool.token1()).approve(address(burveV3), 10_000e18);
-
-        burveV3.mint(address(user), 1000);
-        burveV3.burn(1000);
-
-        vm.stopPrank();
-    }
-
-    // function testCalcLiq() public {
-    //     address poolAddr = 0x246c12D7F176B93e32015015dAB8329977de981B;
-    //     IUniswapV3Pool pool = IUniswapV3Pool(poolAddr);
-
-    //     console.log("tick spacing: ", pool.tickSpacing());
-
-    //     uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(-139);
-    //     uint160 lowerSqrtP = TickMath.getSqrtRatioAtTick(-1000);
-    //     uint160 upperSqrtP = TickMath.getSqrtRatioAtTick(800);
-    //     console.log("lower sqrt: ", upperSqrtP);
-    //     console.log("upper sqrt: ", lowerSqrtP);
-
-    //     (uint256 amount0, uint256 amount1) = LiquidityAmounts
-    //         .getAmountsForLiquidity(sqrtRatioX96, lowerSqrtP, upperSqrtP, 1000);
-    //     console.log("amount0: ", amount0);
-    //     console.log("amount1: ", amount1);
-    // }
-
-    // function testStableMint() public {
-    //     address burveAddr = 0xda5826868528Ace919721ef324a69c16b9486D08;
-    //     address me = address(0x0); // replace with your address
-
-    //     burve = Burve(burveAddr);
-
-    //     IERC20(burve.token0()).approve(burveAddr, 1000);
-    //     IERC20(burve.token1()).approve(burveAddr, 1000);
-
-    //     burve.mint(me, 1000);
-    // }
-
-    // function testIsland() public {
-    //     address me = address(0x0); // replace with your address
-    //     vm.startPrank(me);
-
-    //     IKodiakIsland island = IKodiakIsland(
-    //         BartioAddresses.KODIAK_BERA_YEET_ISLAND_NEW
-    //     );
-
-    //     // TODO(Austin): Not working? Improper setup?
-    //     uint256 balance0 = island.token0().balanceOf(me);
-    //     uint256 balance1 = island.token1().balanceOf(me);
-
-    //     (uint160 sqrtRatioX96, , , , , , ) = island.pool().slot0();
-
-    //     uint128 calcLiq = getLiquidityForAmounts(
-    //         sqrtRatioX96,
-    //         island.lowerTick(),
-    //         island.upperTick(),
-    //         balance0,
-    //         balance1
-    //     );
-
-    //     (uint256 mint0, uint256 mint1) = getAmountsFromLiquidity(
-    //         sqrtRatioX96,
-    //         island.lowerTick(),
-    //         island.upperTick(),
-    //         calcLiq,
-    //         false
-    //     );
-
-    //     (uint256 transfer0, uint256 transfer1, uint256 mintedAmount) = island
-    //         .getMintAmounts(balance0, balance1);
-    //     assert(transfer0 <= balance0);
-    //     assert(transfer1 <= balance1);
-
-    //     island.token0().approve(address(island), transfer0);
-    //     island.token1().approve(address(island), transfer1);
-
-    //     (, , uint128 liqMinted) = island.mint(mintedAmount, me);
-
-    //     assertEq(liqMinted, calcLiq);
-
-    //     vm.stopPrank();
-    // }
-
-    // /**
-    //  * @notice helper function to convert amounts of token0 / token1 to
-    //  * a liquidity value
-    //  * @param sqrtRatioX96 price from slot0
-    //  * @param tickLower bound
-    //  * @param tickUpper bound
-    //  * @param amount0Desired max amount0 available for minting
-    //  * @param amount1Desired max amount1 available for minting
-    //  */
-    // function getLiquidityForAmounts(
-    //     uint160 sqrtRatioX96,
-    //     int24 tickLower,
-    //     int24 tickUpper,
-    //     uint256 amount0Desired,
-    //     uint256 amount1Desired
-    // ) internal pure returns (uint128 liquidity) {
-    //     uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(tickLower);
-    //     uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(tickUpper);
-    //     liquidity = LiquidityAmounts.getLiquidityForAmounts(
-    //         sqrtRatioX96,
-    //         sqrtRatioAX96,
-    //         sqrtRatioBX96,
-    //         amount0Desired,
-    //         amount1Desired
-    //     );
-    // }
-
-    // /**
-    //  * @notice helper function to convert the amount of liquidity to
-    //  * amount0 and amount1
-    //  * @param sqrtRatioX96 price from slot0
-    //  * @param tickLower bound
-    //  * @param tickUpper bound
-    //  * @param liquidity to find amounts for
-    //  * @param roundUp round amounts up
-    //  */
-    // function getAmountsFromLiquidity(
-    //     uint160 sqrtRatioX96,
-    //     int24 tickLower,
-    //     int24 tickUpper,
-    //     uint128 liquidity,
-    //     bool roundUp
-    // ) internal pure returns (uint256 amount0, uint256 amount1) {
-    //     uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(tickLower);
-    //     uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(tickUpper);
-    //     (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
-    //         sqrtRatioX96,
-    //         sqrtRatioAX96,
-    //         sqrtRatioBX96,
-    //         liquidity,
-    //         roundUp
-    //     );
-    // }
 
     // Helpers 
 
@@ -556,5 +484,13 @@ contract BurveTest is ForkableTest {
             liquidity,
             true
         );
+    }
+
+    function shift96(
+        uint256 a,
+        bool roundUp
+    ) internal pure returns (uint256 b) {
+        b = a >> 96;
+        if (roundUp && (a & X96MASK) > 0) b += 1;
     }
 }
