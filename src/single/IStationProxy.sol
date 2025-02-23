c09257f6ba85c85d7ac7517b1f7beac0260caf42
Terence An <terence@itos.fi>
2025-02-23 17:50:20 +0900

    Expand on contract descriptions

--- src/single/IStationProxy.sol
+++ src/single/IStationProxy.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
+/***
+    @notice Station Proxy manages BGT earning and allocation for all of Burve and Burve integrated products.
+
+    Burve protocols typically interact with two LP Tokens: 1. The LP token it issues 2. LP Tokens issued to it
+    from protocols it uses.
+    The protocols send the second type of LP tokens to the station proxy so it can earn BGT on its behalf.
+
+    Protocols do not receive back the earnings from the LP tokens it gives to station proxy directly.
+    They are instead claimed by the user from the station proxy. This is why LP tokens should be ascribed an owner,
+    indicating who can claim the rewards from those LP tokens.
+
+    Exactly how the station proxy directs rewards and BGT earnings is complex and subject to governance.
+***/
 interface IStationProxy {
     /// Called by a user to harvest rewards owed to them from lptoken deposits they own.
     function harvest() external;
