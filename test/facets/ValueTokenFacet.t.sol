7ee4762223806c89d4ab3bd5e6b7a869d1b4e88f
brian <brian@itos.fi>
2025-04-16 13:10:52 -0500

    value token facet tests for minting value

--- test/facets/ValueTokenFacet.t.sol
+++ test/facets/ValueTokenFacet.t.sol
@@ -10,7 +10,7 @@ import {MockERC20} from "../mocks/MockERC20.sol";
 import {MAX_TOKENS} from "../../src/multi/Constants.sol";
 import {IERC20Errors} from "openzeppelin-contracts/interfaces/draft-IERC6093.sol";
 
-contract ValueTokenTest is MultiSetupTest {
+contract ValueTokenFacetTest is MultiSetupTest {
     function setUp() public {
         vm.startPrank(owner);
         _newDiamond();
@@ -23,10 +23,8 @@ contract ValueTokenTest is MultiSetupTest {
     }
 
     function test_ValueTokenNameAndSymbol() public view {
-        // The name and symbol should be constructed from the simplex name and symbol
-        // which are set to "N/A" during diamond initialization
-        string memory expectedName = string.concat("brvValue", "N/A");
-        string memory expectedSymbol = string.concat("val", "N/A");
+        string memory expectedName = "brvValueToken";
+        string memory expectedSymbol = "brvBVT";
 
         // We need to call the functions directly since the constructor isn't called
         assertEq(valueTokenFacet.name(), expectedName);
@@ -35,62 +33,62 @@ contract ValueTokenTest is MultiSetupTest {
 
     function test_MintValue() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 500e18;
+        uint256 tokenValue = 500e18;
 
         // First add some value to the closure for alice
         vm.startPrank(alice);
-        valueFacet.addValue(alice, 3, uint128(value), uint128(bgtValue));
+        valueFacet.addValue(alice, 3, uint128(value), uint128(tokenValue));
         vm.stopPrank();
 
         // Now mint value tokens
         vm.startPrank(alice);
-        valueTokenFacet.mint(value, bgtValue, 3);
+        valueTokenFacet.mint(value, tokenValue, 3);
         vm.stopPrank();
 
         assertEq(valueTokenFacet.balanceOf(alice), value);
     }
 
-    function test_MintValue_RevertWhenBgtValueExceedsValue() public {
+    function test_MintValue_RevertWhenTokenValueExceedsValue() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 1500e18; // BGT value exceeds total value
+        uint256 tokenValue = 1500e18; // Token value exceeds total value
 
         vm.startPrank(alice);
         vm.expectRevert(
             abi.encodeWithSelector(
                 ValueTokenFacet.InsufficientValueForBgt.selector,
                 value,
-                bgtValue
+                tokenValue
             )
         );
-        valueTokenFacet.mint(value, bgtValue, 3);
+        valueTokenFacet.mint(value, tokenValue, 3);
         vm.stopPrank();
     }
 
     function test_BurnValue() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 500e18;
+        uint256 tokenValue = 500e18;
 
         // First add some value to the closure for alice
         vm.startPrank(alice);
-        valueFacet.addValue(alice, 3, uint128(value), uint128(bgtValue));
+        valueFacet.addValue(alice, 3, uint128(value), uint128(tokenValue));
         vm.stopPrank();
 
         // First mint some tokens
         vm.startPrank(alice);
-        valueTokenFacet.mint(value, bgtValue, 3);
+        valueTokenFacet.mint(value, tokenValue, 3);
         vm.stopPrank();
 
         // Then burn them
         vm.startPrank(alice);
-        valueTokenFacet.burn(value, bgtValue, 3);
+        valueTokenFacet.burn(value, tokenValue, 3);
         vm.stopPrank();
 
         assertEq(valueTokenFacet.balanceOf(alice), 0);
     }
 
-    function test_BurnValue_RevertWhenBgtValueExceedsValue() public {
+    function test_BurnValue_RevertWhenTokenValueExceedsValue() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 1500e18; // BGT value exceeds total value
+        uint256 tokenValue = 1500e18; // Token value exceeds total value
 
         // First add some value to the closure for alice
         vm.startPrank(alice);
@@ -102,22 +100,22 @@ contract ValueTokenTest is MultiSetupTest {
         valueTokenFacet.mint(value, 500e18, 3);
         vm.stopPrank();
 
-        // Try to burn with invalid BGT value
+        // Try to burn with invalid token value
         vm.startPrank(alice);
         vm.expectRevert(
             abi.encodeWithSelector(
                 ValueTokenFacet.InsufficientValueForBgt.selector,
                 value,
-                bgtValue
+                tokenValue
             )
         );
-        valueTokenFacet.burn(value, bgtValue, 3);
+        valueTokenFacet.burn(value, tokenValue, 3);
         vm.stopPrank();
     }
 
     function test_BurnValue_RevertWhenInsufficientBalance() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 500e18;
+        uint256 tokenValue = 500e18;
 
         vm.startPrank(alice);
         vm.expectRevert(
@@ -128,22 +126,22 @@ contract ValueTokenTest is MultiSetupTest {
                 value
             )
         );
-        valueTokenFacet.burn(value, bgtValue, 3);
+        valueTokenFacet.burn(value, tokenValue, 3);
         vm.stopPrank();
     }
 
     function test_TransferValue() public {
         uint256 value = 1000e18;
-        uint256 bgtValue = 500e18;
+        uint256 tokenValue = 500e18;
 
         // First add some value to the closure for alice
         vm.startPrank(alice);
-        valueFacet.addValue(alice, 3, uint128(value), uint128(bgtValue));
+        valueFacet.addValue(alice, 3, uint128(value), uint128(tokenValue));
         vm.stopPrank();
 
         // Mint tokens to alice
         vm.startPrank(alice);
-        valueTokenFacet.mint(value, bgtValue, 3);
+        valueTokenFacet.mint(value, tokenValue, 3);
         vm.stopPrank();
 
         // Transfer from alice to bob
@@ -153,5 +151,42 @@ contract ValueTokenTest is MultiSetupTest {
 
         assertEq(valueTokenFacet.balanceOf(alice), 0);
         assertEq(valueTokenFacet.balanceOf(bob), value);
+    }
+
+    function test_MintTransferBurnFlow() public {
+        uint256 value = 1000e18;
+        uint256 tokenValue = 500e18;
+
+        // First add some value to the closure for alice
+        vm.startPrank(alice);
+        valueFacet.addValue(alice, 3, uint128(value), uint128(tokenValue));
+        vm.stopPrank();
+
+        // Mint tokens to alice
+        vm.startPrank(alice);
+        valueTokenFacet.mint(value, tokenValue, 3);
+        vm.stopPrank();
+
+        // Verify alice has the tokens
+        assertEq(valueTokenFacet.balanceOf(alice), value);
+        assertEq(valueTokenFacet.balanceOf(bob), 0);
+
+        // Transfer from alice to bob
+        vm.startPrank(alice);
+        valueTokenFacet.transfer(bob, value);
+        vm.stopPrank();
+
+        // Verify transfer was successful
+        assertEq(valueTokenFacet.balanceOf(alice), 0);
+        assertEq(valueTokenFacet.balanceOf(bob), value);
+
+        // Now bob burns the tokens
+        vm.startPrank(bob);
+        valueTokenFacet.burn(value, tokenValue, 3);
+        vm.stopPrank();
+
+        // Verify final balances
+        assertEq(valueTokenFacet.balanceOf(alice), 0);
+        assertEq(valueTokenFacet.balanceOf(bob), 0);
     }
 }
