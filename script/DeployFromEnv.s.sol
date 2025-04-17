34b7c144b5c1d4e25f2abc8f5a721e8e3eef7adc
brian <brian@itos.fi>
2025-04-17 16:09:46 -0500

    adjustment

---
+++ script/DeployFromEnv.s.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {IDiamond} from "Commons/Diamond/interfaces/IDiamond.sol";
+import {DiamondCutFacet} from "Commons/Diamond/facets/DiamondCutFacet.sol";
+import {InitLib, BurveFacets} from "../src/multi/InitLib.sol";
+import {SimplexDiamond as BurveDiamond} from "../src/multi/Diamond.sol";
+import {SimplexFacet} from "../src/multi/facets/SimplexFacet.sol";
+import {LockFacet} from "../src/multi/facets/LockFacet.sol";
+import {SwapFacet} from "../src/multi/facets/SwapFacet.sol";
+import {ValueFacet} from "../src/multi/facets/ValueFacet.sol";
+import {ValueTokenFacet} from "../src/multi/facets/ValueTokenFacet.sol";
+import {VaultType} from "../src/multi/vertex/VaultProxy.sol";
+import {IAdjustor} from "../src/integrations/adjustor/IAdjustor.sol";
+import {NullAdjustor} from "../src/integrations/adjustor/NullAdjustor.sol";
+
+contract DeployFromEnv is Script {
+    /* Deployer */
+    address deployerAddr;
+
+    uint256 constant INITIAL_MINT_AMOUNT = 1e30;
+    uint128 constant INITIAL_VALUE = 1_000_000e18;
+
+    /* Diamond */
+    address public diamond;
+    ValueFacet public valueFacet;
+    ValueTokenFacet public valueTokenFacet;
+    SimplexFacet public simplexFacet;
+    SwapFacet public swapFacet;
+    LockFacet public lockFacet;
+
+    /* Environment Variables */
+    address[] public tokens;
+    address[] public vaults;
+
+    function run() public {
+        deployerAddr = vm.envAddress("DEPLOYER_PUBLIC_KEY");
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        // Read environment configuration
+        string memory envJson = vm.readFile("script/env.json");
+        tokens = vm.parseJsonAddressArray(envJson, ".tokens");
+        vaults = vm.parseJsonAddressArray(envJson, ".vaults");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        BurveFacets memory facets = InitLib.deployFacets();
+        diamond = address(new BurveDiamond(facets, "ValueToken", "BVT"));
+        console2.log("Burve deployed at:", diamond);
+
+        valueFacet = ValueFacet(diamond);
+        valueTokenFacet = ValueTokenFacet(diamond);
+        simplexFacet = SimplexFacet(diamond);
+        swapFacet = SwapFacet(diamond);
+        lockFacet = LockFacet(diamond);
+
+        // Add vertices for each token and vault pair
+        for (uint256 i = 0; i < tokens.length; ++i) {
+            simplexFacet.addVertex(tokens[i], vaults[i], VaultType.E4626);
+        }
+
+        IAdjustor nAdj = new NullAdjustor();
+        simplexFacet.setAdjustor(address(nAdj));
+
+        // Initialize closures from 3 to 2^n - 1 where n is number of tokens
+        uint16 maxClosure = uint16((1 << tokens.length) - 1);
+        for (uint16 cid = 3; cid <= maxClosure; cid++) {
+            _initializeClosure(cid);
+        }
+
+        vm.stopBroadcast();
+
+        // Log deployed addresses
+        console2.log("Diamond deployed at:", address(diamond));
+        for (uint256 i = 0; i < tokens.length; i++) {
+            console2.log(
+                string.concat("Token", Strings.toString(i), " address:"),
+                tokens[i]
+            );
+            console2.log(
+                string.concat("Vault", Strings.toString(i), " address:"),
+                vaults[i]
+            );
+        }
+
+        // Write addresses to JSON file
+        string memory json = _generateDeploymentJson();
+        vm.writeJson(json, "script/deployment.json");
+    }
+
+    function _generateDeploymentJson() internal view returns (string memory) {
+        string memory json = "{";
+        json = string.concat(
+            json,
+            '"diamond": "',
+            vm.toString(address(diamond)),
+            '",'
+        );
+
+        // Add tokens and vaults arrays
+        json = string.concat(json, '"tokens": [');
+        for (uint256 i = 0; i < tokens.length; i++) {
+            json = string.concat(json, '"', vm.toString(tokens[i]), '"');
+            if (i < tokens.length - 1) json = string.concat(json, ",");
+        }
+        json = string.concat(json, "],");
+
+        json = string.concat(json, '"vaults": [');
+        for (uint256 i = 0; i < vaults.length; i++) {
+            json = string.concat(json, '"', vm.toString(vaults[i]), '"');
+            if (i < vaults.length - 1) json = string.concat(json, ",");
+        }
+        json = string.concat(json, "]");
+
+        json = string.concat(json, "}");
+        return json;
+    }
+
+    /// Initialize a zero fee closure with the initial value amount.
+    function _initializeClosure(uint16 cid) internal {
+        // Mint ourselves enough to fund the initial target of the pool.
+        for (uint256 i = 0; i < tokens.length; ++i) {
+            if ((1 << i) & cid > 0) {
+                // Note: In this version, we assume the deployer already has the tokens
+                // and has approved the diamond contract
+                IERC20(tokens[i]).approve(address(diamond), type(uint256).max);
+            }
+        }
+        simplexFacet.addClosure(cid, INITIAL_VALUE, 0, 0);
+    }
+}
+
+interface IERC20 {
+    function approve(address spender, uint256 amount) external returns (bool);
+}
--- test/facets/Adjustment.t.sol
+++ test/facets/Adjustment.t.sol
@@ -8,6 +8,7 @@ import {IERC4626} from "openzeppelin-contracts/interfaces/IERC4626.sol";
 import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import {VaultType} from "../../src/multi/vertex/VaultProxy.sol";
 import {TransferHelper} from "../../src/TransferHelper.sol";
+import {TokenRegistry, MAX_TOKENS} from "../../src/multi/Token.sol";
 
 contract AdjustmentTest is MultiSetupTest {
     function setUp() public {
@@ -33,17 +34,338 @@ contract AdjustmentTest is MultiSetupTest {
         assertEq(MockERC20(tokens[2]).balanceOf(address(vaults[2])), 100e12);
     }
 
-    function testAdds() public {
-        // Test that valueFacet.addValueSingle and addSingleForValue using 6 decimals on token 2 is the same value as using 18 on token 0.
-        // It'll be roughly the same because the target will change, with each add but they shouldn't be off by 12 decimals of course.
-        // Check balances before and after to double check transfer amounts.
+    function testAddValue() public {
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Add value using both tokens
+        uint128 valueToAdd = 1e18;
+        uint128 bgtValue = 0;
+        valueFacet.addValue(address(this), 0x7, valueToAdd, bgtValue);
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts spent
+        uint256 spent0 = initialBalance0 - finalBalance0;
+        uint256 spent2 = initialBalance2 - finalBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(spent0, spent2 * 1e12, 0.01e18); // Allow 1% difference due to price impact
     }
 
-    function testSwap() public {
+    function testAddValueSingle() public {
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Add value using token0 (18 decimals)
+        uint128 valueToAdd = 1e18;
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[0],
+            0
+        );
+
+        // Add same value using token2 (6 decimals)
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[2],
+            0
+        );
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts spent
+        uint256 spent0 = initialBalance0 - finalBalance0;
+        uint256 spent2 = initialBalance2 - finalBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(spent0, spent2 * 1e12, 0.01e18); // Allow 1% difference due to price impact
+    }
+
+    function testAddSingleForValue() public {
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Add value using token0 (18 decimals)
+        uint128 amountToAdd = 1e18; // 1 token0
+        uint128 minValue = 0;
+        valueFacet.addSingleForValue(
+            address(this),
+            0x7,
+            tokens[0],
+            amountToAdd,
+            0,
+            minValue
+        );
+
+        // Add value using token2 (6 decimals)
+        uint128 amountToAdd2 = 1e6; // 1 token2
+        valueFacet.addSingleForValue(
+            address(this),
+            0x7,
+            tokens[2],
+            amountToAdd2,
+            0,
+            minValue
+        );
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts spent
+        uint256 spent0 = initialBalance0 - finalBalance0;
+        uint256 spent2 = initialBalance2 - finalBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(spent0, spent2 * 1e12, 0.01e18); // Allow 1% difference due to price impact
+    }
+
+    function testRemoveValue() public {
+        // First add some value to remove
+        uint128 valueToAdd = 1e18;
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[0],
+            0
+        );
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[2],
+            0
+        );
+
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Query the total value
+        (
+            uint256 totalValue,
+            uint256 bgtValue,
+            uint256[MAX_TOKENS] memory earnings,
+            uint256 bgtEarnings
+        ) = valueFacet.queryValue(address(this), 0x7);
+
+        // Remove value using both tokens
+        uint128 valueToRemove = uint128(totalValue);
+        uint128 bgtValueToRemove = 0;
+        valueFacet.removeValue(
+            address(this),
+            0x7,
+            valueToRemove,
+            bgtValueToRemove
+        );
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts received
+        uint256 received0 = finalBalance0 - initialBalance0;
+        uint256 received2 = finalBalance2 - initialBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(received0, received2 * 1e12, 0.01e18); // Allow 1% difference due to fees/price impact
+    }
+
+    function testRemoveValueSingle() public {
+        // First add some value to remove
+        uint128 valueToAdd = 1e18;
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[0],
+            0
+        );
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[2],
+            0
+        );
+
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Query the total value
+        (
+            uint256 totalValue,
+            uint256 bgtValue,
+            uint256[MAX_TOKENS] memory earnings,
+            uint256 bgtEarnings
+        ) = valueFacet.queryValue(address(this), 0x7);
+
+        // Remove value using token0 (18 decimals)
+        uint128 valueToRemove = uint128(totalValue / 2);
+        uint128 minReceive0 = 0;
+        valueFacet.removeValueSingle(
+            address(this),
+            0x7,
+            valueToRemove,
+            0,
+            tokens[0],
+            minReceive0
+        );
+
+        // Remove value using token2 (6 decimals)
+        uint128 minReceive2 = 0;
+        valueFacet.removeValueSingle(
+            address(this),
+            0x7,
+            valueToRemove,
+            0,
+            tokens[2],
+            minReceive2
+        );
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts received
+        uint256 received0 = finalBalance0 - initialBalance0;
+        uint256 received2 = finalBalance2 - initialBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(received0, received2 * 1e12, 0.01e18); // Allow 1% difference due to fees/price impact
+    }
+
+    function testRemoveSingleForValue() public {
+        // First add some value to remove
+        uint128 valueToAdd = 1e18;
+        uint128 maxRequired = 1e18;
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[0],
+            0
+        );
+        valueFacet.addValueSingle(
+            address(this),
+            0x7,
+            valueToAdd,
+            0,
+            tokens[2],
+            0
+        );
+
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Remove value using token0 (18 decimals)
+        uint128 amountToRemove = 1e18; // 1 token0
+        uint128 maxValue = type(uint128).max;
+        valueFacet.removeSingleForValue(
+            address(this),
+            0x7,
+            tokens[0],
+            amountToRemove,
+            0,
+            0
+        );
+
+        // Remove value using token2 (6 decimals)
+        uint128 amountToRemove2 = 1e6; // 1 token2
+        valueFacet.removeSingleForValue(
+            address(this),
+            0x7,
+            tokens[2],
+            amountToRemove2,
+            0,
+            0
+        );
+
+        // Check final balances
+        uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Calculate amounts received
+        uint256 received0 = finalBalance0 - initialBalance0;
+        uint256 received2 = finalBalance2 - initialBalance2;
+
+        // Verify amounts are roughly equivalent in value
+        assertApproxEqRel(received0, received2 * 1e12, 0.01e18); // Allow 1% difference due to fees/price impact
+    }
+
+    function testSwapAdjusted() public {
         // Test that swapping 18 decimals of one token gets us 6 decimals of another,
         // but the amount limit happens on nominal values so an 18 decimal limit still works
         // Like this should work even though the actual out amount will be something around 99
         // swapFacet.swap(alice, tokens[0], tokens[2], 1e14, .98e14, 0x7);
         // double check balances before and after for decimal accuracy.
+
+        // Get initial balances
+        uint256 initialBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        uint256 initialBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // Approve tokens for swap
+        MockERC20(tokens[0]).approve(address(diamond), type(uint256).max);
+
+        // Swap from token0 (18 decimals) to token2 (6 decimals)
+        uint256 amountIn = 1e18; // 1 token0
+        uint256 amountLimit = 98e16; // 0.98 token2 (in 6 decimals)
+        swapFacet.swap(
+            address(this),
+            tokens[0],
+            tokens[2],
+            int256(amountIn),
+            amountLimit,
+            0x7
+        );
+
+        // Check final balances
+        // uint256 finalBalance0 = MockERC20(tokens[0]).balanceOf(address(this));
+        // uint256 finalBalance2 = MockERC20(tokens[2]).balanceOf(address(this));
+
+        // // Verify the swap amounts
+        // uint256 spent0 = initialBalance0 - finalBalance0;
+        // uint256 received2 = finalBalance2 - initialBalance2;
+
+        // assertEq(spent0, amountIn, "Incorrect amount spent");
+        // assertTrue(
+        //     received2 >= amountLimit,
+        //     "Received less than minimum amount"
+        // );
+
+        // // Now swap back from token2 to token0
+        // MockERC20(tokens[2]).approve(address(diamond), type(uint256).max);
+        // uint256 amountIn2 = 1e6; // 1 token2
+        // uint256 amountLimit2 = 98e12; // 0.98 token0 (in 18 decimals)
+        // swapFacet.swap(
+        //     address(this),
+        //     tokens[2],
+        //     tokens[0],
+        //     int256(amountIn2),
+        //     amountLimit2,
+        //     0x7
+        // );
     }
 }
