b88550998e5958ff75d9af9aaebab0fbbefe680d
urlaubaitos <austin@itos.fi>
2025-05-29 19:25:24 -0400

    update SwapRouter after RFT changes in SwapFacet

--- src/integrations/router/SwapRouter.sol
+++ src/integrations/router/SwapRouter.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
-import {SwapFacet} from "../../../src/multi/facets/SwapFacet.sol";
+import {Auto165} from "Commons/ERC/Auto165.sol";
+import {IRFTPayer, RFTPayer} from "Commons/Util/RFT.sol";
+import {IBurveMultiSwap} from "../../multi/interfaces/IBurveMultiSwap.sol";
+import {TransferHelper} from "../../TransferHelper.sol";
 
 /// @notice Defines the swap route per closure.
 struct Route {
@@ -12,11 +15,28 @@ struct Route {
 }
 
 /// Swap Router. For handling multiple swaps over multiple closures.
-contract SwapRouter {
+contract SwapRouter is RFTPayer, Auto165 {
     error ExcessiveAmountIn(uint256 acceptable, uint256 actual);
     error InsufficientAmountOut(uint256 acceptable, uint256 actual);
+    error ReentrancyAttempt();
+    error UntrustedTokenRequest(address sender);
+
+    // Transient state;
+    bool transient tLocked;
+    address transient tPayer;
+    address transient tSwapper;
+
+    /// @notice Prevents reentrancy by locking the contract during the call.
+    ///         When locked all nonReentrant functions will revert.
+    modifier nonReentrant {
+        require(!tLocked, ReentrancyAttempt());
+        tLocked = true;
+        _;
+        tLocked = false;
+    }
 
     /// @notice Single hop token swap over multiple closures.
+    /// @param swapper The address of the swapper.
     /// @param recipient The recipient of the outToken.
     /// @param inToken Token to swap in.
     /// @param outToken Token to swap out.
@@ -27,24 +47,30 @@ contract SwapRouter {
     /// Exact Input - route amountSpecified values are positive, amountLimit is positive to confirm minimum amount out.
     /// Exact Output - route amountSpecified values are negative, amountLimit is negative to confirm maximum amount in.
     function swap(
-        address diamond,
+        address swapper,
         address recipient,
         address inToken,
         address outToken,
         int256 amountLimit,
         Route[] calldata routes
-    ) external returns (uint256 inAmount, uint256 outAmount) {
+    ) external nonReentrant returns (uint256 inAmount, uint256 outAmount) {
+        // Update transient storage 
+        tSwapper = swapper;
+        tPayer = msg.sender;
+
+        // Execute swaps
         for (uint256 i = 0; i < routes.length; ++i) {
             Route memory route = routes[i];
 
-            (uint256 _inAmount, uint256 _outAmount) = SwapFacet(diamond).swap(
-                recipient,
-                inToken,
-                outToken,
-                route.amountSpecified,
-                0,
-                route.cid
-            );
+            (uint256 _inAmount, uint256 _outAmount) = IBurveMultiSwap(swapper)
+                .swap(
+                    address(this),
+                    inToken,
+                    outToken,
+                    route.amountSpecified,
+                    0,
+                    route.cid
+                );
             inAmount += _inAmount;
             outAmount += _outAmount;
         }
@@ -62,6 +88,44 @@ contract SwapRouter {
                 outAmount >= minOut,
                 InsufficientAmountOut(minOut, outAmount)
             );
+        }
+
+        // Transfer to the recipient
+        if (outAmount > 0) {
+            TransferHelper.safeTransfer(
+                outToken,
+                recipient,
+                outAmount
+            );
+        }
+
+        // Reset transient storage
+        tSwapper = address(0x0);
+        tPayer = address(0x0);
+    }
+
+    /// @inheritdoc IRFTPayer
+    function tokenRequestCB(
+        address[] calldata tokens,
+        int256[] calldata requests,
+        bytes calldata data
+    ) external returns (bytes memory cbData) {
+        require(msg.sender == tSwapper && tSwapper != address(0x0), UntrustedTokenRequest(msg.sender));
+
+        for (uint256 i = 0; i < tokens.length; ++i) {
+            int256 amount = requests[i];
+
+            // Requested payment
+            if (amount > 0) {
+                address token = tokens[i];
+
+                TransferHelper.safeTransferFrom(
+                    token,
+                    tPayer,
+                    msg.sender,
+                    uint256(amount)
+                );
+            }
         }
     }
 }
