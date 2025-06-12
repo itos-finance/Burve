0b90c03e748cbe222a0b0cbf268e4909779db078
ItosTerence <115845392+ItosTerence@users.noreply.github.com>
2025-06-12 17:09:14 -0400

    Thorough changes to Opener (#85)

--- src/integrations/opener/Opener.sol
+++ src/integrations/opener/Opener.sol
@@ -11,137 +11,157 @@ import {FullMath} from "../../FullMath.sol";
 import {MAX_TOKENS} from "../../multi/Constants.sol";
 import {console2} from "forge-std/console2.sol";
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuardTransient} from "openzeppelin-contracts/utils/ReentrancyGuardTransient.sol";
+import {SafeCast} from "Commons/Math/Cast.sol";
 
-contract Opener is IRFTPayer {
-    address BEPOLIA_EXECUTOR = 0xADEC0cE4efdC385A44349bD0e55D4b404d5367B4;
-    address BERACHAIN_EXECUTOR = 0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7;
+address constant BEPOLIA_EXECUTOR = 0xADEC0cE4efdC385A44349bD0e55D4b404d5367B4;
+address constant BERACHAIN_EXECUTOR = 0xFd88aD4849BA0F729D6fF4bC27Ff948Ab1Ac3dE7;
 
-    IOBRouter router;
-    uint256[] amountsOut;
+contract Opener is IRFTPayer, ReentrancyGuardTransient {
+    address public immutable executor;
+    address public transient _pool;
 
-    constructor() {
-        router = IOBRouter(BERACHAIN_EXECUTOR);
+    constructor(address _executor) {
+        executor = _executor;
     }
 
+    error InvalidPool();
     error InvalidCaller();
+    error InvalidRequest();
+    error AmountSlippageExceeded();
+    error ValueSlippageExceeded();
     error OogaBoogaFailure();
 
     function mint(
         address pool,
-        address[] calldata tokens,
-        bytes[] calldata txData,
+        uint8 inTokenIdx,
+        uint256 inAmount,
+        bytes[MAX_TOKENS] calldata txData,
         uint16 closureId,
-        uint256 nonSwappingAmount,
         uint256 bgtPercentX256,
-        uint256[MAX_TOKENS] memory amountLimits
-    ) external {
-        amountsOut = new uint256[](tokens.length);
-        amountsOut[0] = nonSwappingAmount;
+        uint256[MAX_TOKENS] calldata amountLimits,
+        uint256 minValueReceived
+    ) external nonReentrant returns (uint256 addedValue) {
+        _pool = pool;
 
-        /// note we start from index 1
-        for (uint256 i = 0; i < txData.length; i++) {
-            IERC20(tokens[i + 1]).approve(
-                BERACHAIN_EXECUTOR,
-                nonSwappingAmount
+        address[] memory tokens = IBurveMultiSimplex(pool)
+            .getTokens();
+        require(tokens.length <= MAX_TOKENS, InvalidPool());
+        uint256[] memory myBalances = new uint256[](tokens.length);
+
+        // Get the tokens ready to send to the OB executor.
+        TransferHelper.safeTransferFrom(
+            tokens[inTokenIdx],
+            msg.sender,
+            address(this),
+            inAmount
+        );
+        IERC20(tokens[inTokenIdx]).approve(
+                executor,
+                inAmount
             );
-            (bool success, bytes memory data) = BEPOLIA_EXECUTOR.call(
-                txData[i]
-            );
 
-            console2.log("success", success);
-            // console2.logBytes("data", data);
+        // Now get all our balances.
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (i == inTokenIdx) {
+                continue;
+            }
 
+            // Skip tokens we don't want.
+            if (txData[i].length == 0) continue;
+
+            // Swap for the tokens we do.
+            (bool success, bytes memory data) = executor.call(txData[i]);
             if (!success) revert OogaBoogaFailure();
 
             // store the amountOut from the oogabooga swap
-            amountsOut[i + 1] = IERC20(tokens[i + 1]).balanceOf(address(this)); // abi.decode(data, (uint256));
+            myBalances[i] = IERC20(tokens[i]).balanceOf(address(this));
         }
+        myBalances[inTokenIdx] = IERC20(tokens[inTokenIdx]).balanceOf(address(this));
 
-        console2.log("here");
-        // calculate the max value to add
+        // Determine how much value we can add.
+        {
         (
             uint8 n,
             uint256 targetX128,
-            uint256[MAX_TOKENS] memory balances,
+            uint256[MAX_TOKENS] memory closureBalances,
             ,
 
         ) = IBurveMultiSimplex(pool).getClosureValue(closureId);
 
         address adjustor = IBurveMultiSimplex(pool).getAdjustor();
-        uint256[] memory closureBalances = new uint256[](n);
-        uint256 j = 0;
-        for (uint256 i = 0; i < balances.length; i++) {
-            if (balances[i] == 0) continue;
-            closureBalances[j] = IAdjustor(adjustor).toReal(
-                tokens[j],
-                balances[i],
+        uint256 minPercentX256 = type(uint256).max;
+        for (uint256 i = 0; i < closureBalances.length; i++) {
+            if (closureBalances[i] == 0) continue;
+            // We know closure balances line up with tokens
+            uint256 realBalance = IAdjustor(adjustor).toReal(
+                tokens[i],
+                closureBalances[i],
                 true
             );
-            j++;
-        }
-
-        uint256[] memory percentagesX128 = new uint256[](n);
-        for (uint256 i = 0; i < closureBalances.length; i++) {
-            percentagesX128[i] = (amountsOut[i] << 128) / closureBalances[i];
-        }
-
-        uint256 minValue = type(uint256).max;
-        for (uint256 i = 0; i < percentagesX128.length; i++) {
-            uint256 target = (targetX128 * percentagesX128[i]) /
-                (1 << 128) /
-                (1 << 128);
-            uint256 value = target * n;
-            if (value < minValue) {
-                minValue = value;
+            uint256 percentX256 = FullMath.mulDivX256(myBalances[i], realBalance, false);
+            if (percentX256 < minPercentX256) {
+                minPercentX256 = percentX256;
             }
         }
 
-        uint256 bgtValue = FullMath.mulX256(bgtPercentX256, minValue, true);
+        // round the value we add down to make sure we fit.
+        addedValue = FullMath.mulX128(
+            FullMath.mulX256(targetX128, minPercentX256, false),
+            n,
+            false);
+        }
+
+        // Round up to handle the 100% case exactly.
+        uint256 bgtValue = FullMath.mulX256(bgtPercentX256, addedValue, true);
 
         IBurveMultiValue(pool).addValue(
             msg.sender,
             closureId,
-            uint128(minValue),
+            SafeCast.toUint128(addedValue),
             uint128(bgtValue),
             amountLimits
         );
 
-        // single deposit any remaining amounts of each token
-        for (uint256 i = 0; i < amountsOut.length; i++) {
-            if (amountsOut[i] > 0) {
-                IBurveMultiValue(pool).addSingleForValue(
+        // Single deposit any remaining amounts of each token
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uint128 balance = SafeCast.toUint128(IERC20(tokens[i]).balanceOf(address(this)));
+            if (balance > 0) {
+                addedValue += IBurveMultiValue(pool).addSingleForValue(
                     msg.sender,
                     closureId,
                     tokens[i],
-                    uint128(amountsOut[i]),
+                    balance,
                     bgtPercentX256, // lazy
                     0
                 );
-                amountsOut[i] = 0;
             }
         }
+
+        if (addedValue < minValueReceived) {
+            revert ValueSlippageExceeded();
+        }
+
+        // Even though this will be cleared after the transaction, we still clear it now
+        // as a pseudo-entrancy-check.
+        _pool = address(0);
     }
 
+    /// Simple RFT compliant payment callback that always pays debts.
     function tokenRequestCB(
         address[] calldata tokens,
         int256[] calldata requests,
-        bytes calldata data
-    ) external returns (bytes memory cbData) {
-        /// if the tokens length is one, its a addSingleForValue deposit, therefore we don't need to do the additional accounting
-        if (tokens.length == 1) {
-            TransferHelper.safeTransfer(
-                tokens[0],
-                msg.sender,
-                uint256(requests[0])
-            );
-        }
+        bytes calldata
+    ) external returns (bytes memory _cbData) {
+        // Let's check we're being called by who we expect.
+        require(msg.sender == _pool, InvalidRequest());
+
         for (uint256 i = 0; i < tokens.length; i++) {
             TransferHelper.safeTransfer(
                 tokens[i],
                 msg.sender,
-                uint256(requests[i])
+                SafeCast.toUint256(requests[i])
             );
-            amountsOut[i] -= uint256(requests[i]);
         }
     }
 }
